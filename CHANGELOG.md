a485ebf94400432d84c852c6c9b5a95003249712
Daniel Varga <dvarga@skawa.hu>
2016-11-19 18:39:03 +0100

    dependency package paths are searched for @imports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.0.3
+
+* **experimental**: package dependency paths are search by sass for `@import`s
+
 ## v0.0.2
 
 * travis integration
--- lib/sass_builder.dart
+++ lib/sass_builder.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 
+import 'dart:io';
 import 'package:build/build.dart';
+import 'package:package_config/packages.dart';
 import 'package:path/path.dart' as p;
 import 'package:sass/src/ast/sass.dart';
 import 'package:sass/src/visitor/perform.dart';
 import 'package:sass/src/visitor/serialize.dart';
+import 'package:package_config/discovery.dart';
 
-String _renderScss(String contents, {Uri url, bool color: false}) {
+Future<String> _renderScss(String contents, {Uri url, bool color: false, List<String> loadPaths}) async {
   var sassTree = new Stylesheet.parseScss(contents, url: url, color: color);
-  var cssTree = evaluate(sassTree, color: color);
+  var cssTree = evaluate(sassTree, loadPaths: loadPaths, color: color);
   return toCss(cssTree);
 }
 
@@ -18,7 +21,9 @@ class SassBuilder extends Builder {
   Future build(BuildStep buildStep) async {
     var input = buildStep.input;
     var url = p.toUri(input.id.path);
-    var css = _renderScss(input.stringContents, url: url);
+    var packages = await _dependencies;
+    var paths = packages.asMap().values.map((Uri uri) => uri.toFilePath()).toList();
+    var css = await _renderScss(input.stringContents, url: url, loadPaths: paths);
     var cssAsset = new Asset(_toCompiledSassAsset(input.id), css);
     await buildStep.writeAsString(cssAsset);
   }
@@ -27,6 +32,16 @@ class SassBuilder extends Builder {
   List<AssetId> declareOutputs(AssetId inputId) {
     return [_toCompiledSassAsset(inputId)];
   }
+
+  Future<Packages> get _dependencies async {
+    // let's cache
+    if(_packages == null) {
+      _packages = await findPackages(Platform.script);
+    }
+    return _packages;
+  }
+
+  Packages _packages;
 
   static AssetId _toCompiledSassAsset(AssetId inputId) => inputId.changeExtension('.css');
 
